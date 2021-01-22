9ea744cbb72b444a4942e8c301c328cf0a28af04
PiyaJaju <72734119+PiyaJaju@users.noreply.github.com>
2021-01-22 18:14:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var dustbinObj, paperObject,groundObject	
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	dustbinObj=new Dustbin(1100,650);
+	paperObject=new Paper(200,450,60);
+	groundObject=new Ground(width/2,670,width,20);
+	
+	
+	
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+  Engine.update(engine)
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
+  
+ 
+  
+  
+ 
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	}
+}
+  
+ 
+
+
+
+
